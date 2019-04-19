ef0a243134376032f30575046ff6755fd489acad
AP63049 <andrei.popovskii@citi.com>
2019-04-19 17:43:04 +0300

    refactor(actions): Simplify custom thunk typings

--- src/components/sample/sample.actions.ts
+++ src/components/sample/sample.actions.ts
@@ -1,8 +1,8 @@
-import { ActionType, createAsyncAction } from "typesafe-actions";
+import { createAsyncAction } from "typesafe-actions";
 import { SampleData } from "./sample.model";
 import { ThunkExtraArguments } from "../../store";
-import { ThunkAction, ThunkDispatch } from "redux-thunk";
 import { RootState } from "../../store/reducer";
+import { AsyncThunkAction, RootDispatch } from "ThunkTypes";
 
 export const sampleAsyncAction = createAsyncAction("@sample/REQUEST", "@sample/SUCCESS", "@sample/FAIL")<
   void,
@@ -10,40 +10,12 @@ export const sampleAsyncAction = createAsyncAction("@sample/REQUEST", "@sample/S
   Error
 >();
 
-let body = async function(
-  dispatch: ThunkDispatch<RootState, ThunkExtraArguments, ActionType<typeof sampleAsyncAction>>,
-  getState: () => RootState,
-  extraArguments: ThunkExtraArguments
-) {
-  dispatch(sampleAsyncAction.request());
-
-  try {
-    const data: SampleData[] = await extraArguments.api.fetchData();
-    return dispatch(sampleAsyncAction.success(data));
-  } catch (e) {
-    dispatch(sampleAsyncAction.failure(e));
-    return Promise.reject();
-  }
-};
-export const fetchSomeData = (): ThunkAction<
-  ReturnType<typeof body>,
-  any,
-  ThunkExtraArguments,
-  ActionType<typeof sampleAsyncAction>
-> => body;
-
-/*
-export const fetchSomeData = (): ThunkAction<
-  Promise<PayloadAction<"@sample/SUCCESS", SampleData[]>>,
-  any,
-  ThunkExtraArguments,
-  AnyAction
-  > =>
-  async function(
-    dispatch: GenericThunkDispatch<typeof sampleAsyncAction>,
+export function fetchSomeData(): AsyncThunkAction<typeof sampleAsyncAction.success> {
+  return async function(
+    dispatch: RootDispatch,
     getState: () => RootState,
     extraArguments: ThunkExtraArguments
-  ) {
+  ): Promise<ReturnType<typeof sampleAsyncAction.success>> {
     dispatch(sampleAsyncAction.request());
 
     try {
@@ -54,4 +26,4 @@ export const fetchSomeData = (): ThunkAction<
       return Promise.reject();
     }
   };
-*/
+}
--- src/store/typings.d.ts
+++ src/store/typings.d.ts
@@ -1,16 +1,17 @@
 declare module "ThunkTypes" {
   import { ThunkAction, ThunkDispatch } from "redux-thunk";
-  import { ActionType } from "typesafe-actions";
   import { ThunkExtraArguments } from "./index";
-  import { Action, AnyAction } from "redux";
-  import { RootState } from "Store";
+  import { RootAction, RootState } from "Store";
+  import { AnyAction } from "redux";
 
-  export type GenericThunkAction<T> = ThunkAction<Promise<Action>, any, ThunkExtraArguments, AnyAction>;
-  export type GenericThunkDispatch<T> = ThunkDispatch<RootState, ThunkExtraArguments, ActionType<T>>;
+  export type AsyncThunkAction<R> = ThunkAction<Promise<ReturnType<R>>, any, ThunkExtraArguments, AnyAction>;
+  export type SyncThunkAction<R> = ThunkAction<ReturnType<R>, any, ThunkExtraArguments, AnyAction>;
+
+  export type RootDispatch = ThunkDispatch<RootState, ThunkExtraArguments, RootAction>;
 }
 
 declare module "Store" {
-  import { ActionType, StateType } from "typesafe-actions";
+  import { ActionType } from "typesafe-actions";
 
   export type RootAction = ActionType<typeof import("./action")>;
   // export type RootState = StateType<typeof import("./reducer").default>;
